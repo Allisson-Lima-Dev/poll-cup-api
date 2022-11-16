cc0d213a5329d5eb4af8faeca56ef6c82c659fd4
Allisson Lima <Allysoncosta@outlook.com>
2022-11-16 10:52:53 -0300

    feat: add route create user

--- src/routes/users/index.ts
+++ src/routes/users/index.ts
@@ -1,4 +1,5 @@
 import { FastifyInstance, FastifyRequest } from "fastify";
+import fastifyBcrypt from "fastify-bcrypt";
 import { z, AnyZodObject } from "zod";
 import { prisma } from "../../lib/prisma";
 
@@ -32,7 +33,13 @@ export class Users {
     fastify.get("/users/count", async (req, res) => {
       const counts = await prisma.user.count();
 
-      return { count: counts };
+      const passwordHash = await fastify.bcrypt.hash("123");
+      const checkPassword = await fastify.bcrypt.compare("123", passwordHash);
+      return {
+        count: counts,
+        passwordHash,
+        checkPassword: !checkPassword ? "Senha Invalida" : "Correta 🥳",
+      };
     });
   }
 }
@@ -48,26 +55,87 @@ export class UserCreate {
             })
             .email("Email inválido"),
           name: z.string({
-            required_error: "Name Obrigatório",
+            required_error: "Nome Obrigatório",
+          }),
+          password: z.string({
+            required_error: "Senha Obrigatório",
           }),
           picture: z.string().url().optional(),
         });
 
-        const userBody = userSchema.parse(req.body);
+        const { password, picture, ...rest } = userSchema.parse(req.body);
+        const passwordHash = await fastify.bcrypt.hash(password);
 
         let user = await prisma.user.create({
           data: {
-            name: userBody.name,
+            password: passwordHash,
             avatarUrl:
-              userBody.picture ||
+              picture ||
               "https://cdn.icon-icons.com/icons2/1378/PNG/512/avatardefault_92824.png",
-            email: userBody.email,
+            ...rest,
           },
         });
 
         return res.status(201).send({
           user,
         });
+      } catch (error) {
+        return res.status(500).send({
+          error,
+        });
+      }
+    });
+  }
+}
+
+export class SignInCredentials {
+  async signIn(fastify: FastifyInstance) {
+    fastify.post("/signIn", async (req, res) => {
+      try {
+        const signInSchema = z.object({
+          email: z
+            .string({
+              required_error: "Email Obrigatório",
+            })
+            .email("Email inválido"),
+          password: z.string({
+            required_error: "Senha Obrigatório",
+          }),
+        });
+        const { email, password } = signInSchema.parse(req.body);
+
+        let user = await prisma.user.findUnique({
+          where: {
+            email: email,
+          },
+        });
+
+        if (!user) {
+          return res.status(404).send({ result: "Usuário não encontrado" });
+        }
+
+        const checkPassword = await fastify.bcrypt.compare(
+          password,
+          user?.password || ""
+        );
+        if (!checkPassword) {
+          return res.status(404).send({ result: "Credências incorretas" });
+        }
+
+        const token = fastify.jwt.sign(
+          {
+            name: user?.name,
+            email: user?.email,
+            avatarUrl: user?.avatarUrl,
+          },
+          {
+            sub: user?.id,
+            expiresIn: "1 days",
+          }
+        );
+        return {
+          token,
+        };
       } catch (error) {
         return res.status(500).send({
           error,
