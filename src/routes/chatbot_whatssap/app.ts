ca44ac30330dff469c1389d9d19319f1b8e854bd
Allisson Lima <Allysoncosta@outlook.com>
2022-11-20 16:29:52 -0300

    feat: add bot whatssap get QRcode

--- src/routes/chatbot_whatssap/app.ts
+++ src/routes/chatbot_whatssap/app.ts
@@ -1,15 +1,16 @@
 import { FastifyInstance } from "fastify";
-// import { client } from "../../lib/bot_whatssap";
+import { client } from "../../lib/bot_whatssap";
+
 export class BotQRcode {
   async getQRcode(fastify: FastifyInstance) {
     let code = "";
     fastify.get("/Qrcode", async (req, res) => {
-      // client.on("qr", (qr) => {
-      //   console.log(qr);
-      //   // Generate and scan this code with your phone
-      //   code = qr;
-      //   console.log("QR RECEIVED", qr);
-      // });
+      client.on("qr", (qr) => {
+        console.log(qr);
+        // Generate and scan this code with your phone
+        code = qr;
+        console.log("QR RECEIVED", qr);
+      });
       return { QRcode: code };
     });
   }
