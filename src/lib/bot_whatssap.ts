54428bf0caf81e69acf9afe8c0dbbedc8b8ecd08
Allisson Lima <Allysoncosta@outlook.com>
2022-11-21 15:30:17 -0300

    feat: add peppeteer

--- src/lib/bot_whatssap.ts
+++ src/lib/bot_whatssap.ts
@@ -2,4 +2,9 @@ import { Client, LocalAuth, NoAuth } from "whatsapp-web.js";
 
 export const client = new Client({
   authStrategy: new LocalAuth(),
+  puppeteer: {
+    executablePath: "/usr/bin/chromium-browser",
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    headless: false,
+  },
 });
